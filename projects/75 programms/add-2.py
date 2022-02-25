ad6dba3e7518eac843bd87a9a9b2e748727da9bf
amartuty <98328700+amartuty@users.noreply.github.com>
2022-02-25 17:51:36 +0530

    Add files via upload

---
+++ projects/75 programms/add-2.py
@@ -0,0 +1,16 @@
+#Prg1
+#addition of two numbers
+#method1
+
+a =56
+b =560
+sum = a+b
+print(sum)
+
+#addition of two numbers
+#method2
+#get no from users
+a =int(input("enter the no1:"))
+b =int(input("enter the no2:"))
+sum = a+b
+print(sum)
